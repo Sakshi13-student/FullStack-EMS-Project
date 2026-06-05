c439bd53b63a6c4f485aa2a927a1fc20bdd60770
Sakshi Chourasia <chourasiasakshi16@gmail.com>
2026-06-06 02:14:20 +0530

    API created for leave application

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,11 +10,12 @@ export const login = async (req, res) => {
         const {email, password, role_type} = req.body;
         
         if(!email || !password) {
-            return res.status(400).json({error: "Email and password"})
+            return res.status(400).json({error: "Email and password are required"})
         }
         const user = await User.findOne({email})
         if(!user) {
-            return res.status(401).json({ error: "Invalid credentials" });        }
+            return res.status(401).json({ error: "Invalid credentials" });        
+        }
 
         if(role_type === "admin" && user.role !== "ADMIN") {
             return res.status(401).json({error: "Not authorized as admin"});
