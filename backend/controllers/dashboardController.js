823d38771a5c97a8d0636e2081d4b4741b24b8c3
Sakshi Chourasia <chourasiasakshi16@gmail.com>
2026-06-09 02:05:50 +0530

    API created for dashboard data

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,8 +1,8 @@
-import { DEPARTMENTS } from "../constants/departments";
-import Attendance from "../models/Attendance";
-import Employee from "../models/Employee";
-import LeaveApplication from "../models/LeaveApplication";
-import Payslip from "../models/Payslip";
+import { DEPARTMENTS } from "../constants/departments.js";
+import Attendance from "../models/Attendance.js";
+import Employee from "../models/Employee.js";
+import LeaveApplication from "../models/LeaveApplication.js";
+import Payslip from "../models/Payslip.js";
 
 // Get dashboard for employee and admin
 // GET /api/dashboard
